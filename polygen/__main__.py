ba75ae60f50bddf9ab7ab0275a917f2d6664f5dd
glebzlat <mrkotofey@inbox.ru>
2024-04-21 11:35:56 +0500

    Add simple module functionality

--- polygen/__main__.py
+++ polygen/__main__.py
@@ -1,7 +1,22 @@
+import sys
+
+from argparse import ArgumentParser, FileType
+
 from .reader import Reader
+from .grammar_parser import GrammarParser
+
+argparser = ArgumentParser()
+argparser.add_argument("file", nargs='?', type=FileType('r', encoding='utf-8'),
+                       default=sys.stdin)
 
 
 def main():
+    ns = argparser.parse_args()
+
+    reader = Reader(ns.file)
+    parser = GrammarParser(reader)
+    print(parser.parse())
+
     return 0
 
 
