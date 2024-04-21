381ca350d23ed23884bf6f8d0f28e97ff9d7e5b3
glebzlat <mrkotofey@inbox.ru>
2024-04-21 16:24:47 +0500

    Add --str, --repr and --descendants args
    It will print user string representation with `--str`, representation
    using `repr` with `--repr` and all nodes recursively with `--descendants`.

--- polygen/__main__.py
+++ polygen/__main__.py
@@ -8,6 +8,10 @@ from .grammar_parser import GrammarParser
 argparser = ArgumentParser()
 argparser.add_argument("file", nargs='?', type=FileType('r', encoding='utf-8'),
                        default=sys.stdin)
+repr_types = argparser.add_mutually_exclusive_group()
+repr_types.add_argument("--str", "-s", action='store_true')
+repr_types.add_argument("--repr", "-r", action='store_true')
+repr_types.add_argument("--descendants", "-d", action='store_true')
 
 
 def main():
@@ -15,7 +19,15 @@ def main():
 
     reader = Reader(ns.file)
     parser = GrammarParser(reader)
-    print(parser.parse())
+    grammar = parser.parse()
+
+    if ns.repr:
+        print(repr(grammar))
+    elif ns.descendants:
+        for node in map(repr, grammar.descendants):
+            print(node)
+    else:
+        print(grammar)
 
     return 0
 
