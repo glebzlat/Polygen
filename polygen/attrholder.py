622a913f5d587ac744b7f8c7c565b2af3f9061a5
glebzlat <mrkotofey@inbox.ru>
2024-04-21 11:33:39 +0500

    Add `AttributeHolder._repr` method
    Method accepts `repr_fn` parameter, which is a function used to
    convert arguments to strings. This allows to use either `repr` or
    `str` functions.

--- polygen/attrholder.py
+++ polygen/attrholder.py
@@ -1,20 +1,28 @@
+from typing import Callable, Any
+
+
 class AttributeHolder:
     def __repr__(self):
+        return self._repr(repr)
+
+    def __str__(self):
+        return self._repr(str)
+
+    def _repr(self, repr_fn: Callable[[Any], str]) -> str:
         type_name = type(self).__name__
-        arg_strings = [repr(arg) for arg in self._get_args()]
+        arg_strings = [repr_fn(arg) for arg in self._get_args()]
         star_args = {}
         for name, value in self._get_kwargs():
             if name.isidentifier():
-                arg_strings.append(f"{name}={value!r}")
+                value_repr = repr_fn(value)
+                arg_strings.append(f"{name}={value_repr}")
             else:
                 star_args[name] = value
         if star_args:
-            arg_strings.append(f"**{star_args:r}")
+            args_repr = repr_fn(star_args)
+            arg_strings.append(f"**{args_repr}")
         args = ', '.join(arg_strings)
         return f"{type_name}({args})"
-
-    def __str__(self):
-        return self.__repr__()
 
     def _get_kwargs(self):
         return list(self.__dict__.items())
