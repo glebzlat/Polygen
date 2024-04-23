f5d5aada2c2be6480066c5d6fa37178e43e467f8
glebzlat <mrkotofey@inbox.ru>
2024-04-23 12:29:53 +0500

    Add __len__ method for sized nodes and inherit them from Sized; inherit Node from Iterable; add common_prefix and has_prefix functions
    Add methods: Grammar.__len__, Expression.__add__, Expression.__len__,
    Rule.__len__, Add.__len__, Add.__add__, Part.__len__, Class.__len__,
    and comparison methods to Class.
    
    Import Iterable and Sized classes from collections.abc, as their aliases
    from typing are deprecated.

--- polygen/node.py
+++ polygen/node.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 import string
 
-from typing import Iterable, Optional, Any
+from typing import Optional, Any
+from collections.abc import Iterable, Sized
 from enum import StrEnum
 from itertools import chain
 
 from .attrholder import AttributeHolder, ArgsRepr
 
 
-class Node:
+class Node(Iterable):
     @property
     def descendants(self):
         def iterate():
@@ -27,18 +28,21 @@ class Node:
         yield self
 
 
-class Grammar(Node, AttributeHolder):
+class Grammar(Node, AttributeHolder, Sized):
     def __init__(self, rules: Iterable[Rule]):
         self.rules = {rule.name: rule for rule in rules}
-
-    def __str__(self):
-        return '\n'.join(map(str, self.rules.values()))
 
     def __iter__(self):
         yield from self.rules.values()
 
+    def __len__(self):
+        return len(self.rules)
 
-class Expression(Node, ArgsRepr):
+    def __str__(self):
+        return '\n'.join(map(str, self.rules.values()))
+
+
+class Expression(Node, ArgsRepr, Sized):
     def __init__(self, alts: Iterable[Alt]):
         self.alts = alts
 
@@ -53,12 +57,20 @@ class Expression(Node, ArgsRepr):
             return NotImplemented
         return self.alts == other.alts
 
+    def __add__(self, other):
+        if not isinstance(other, Expression):
+            raise TypeError
+        return Expression(list(self.alts) + list(other.alts))
+
+    def __len__(self):
+        return len(self.alts)
+
     def __str__(self):
         alts = ', '.join(map(str, self.alts))
         return f"Expression({alts})"
 
 
-class Rule(Node, ArgsRepr):
+class Rule(Node, ArgsRepr, Sized):
     def __init__(self, name: Identifier, rhs: Expression):
         self.name = name
         self.rhs = rhs
@@ -66,13 +78,16 @@ class Rule(Node, ArgsRepr):
     def _get_args(self):
         return [self.name, self.rhs]
 
+    def __iter__(self):
+        yield self.rhs
+
+    def __len__(self):
+        return len(self.rhs)
+
     def __eq__(self, other):
         if not isinstance(other, Rule):
             return NotImplemented
         return (self.name, self.rhs) == (other.name, other.rhs)
-
-    def __iter__(self):
-        yield self.rhs
 
 
 class Identifier(Node, ArgsRepr):
@@ -91,12 +106,12 @@ class Identifier(Node, ArgsRepr):
         return hash(self.string)
 
 
-class Alt(Node, ArgsRepr):
+class Alt(Node, ArgsRepr, Sized):
     def __init__(self, parts: Iterable[Part]):
         self.parts = parts
 
     def _get_args(self):
-        return list(self.parts)
+        return [self.parts]
 
     def __eq__(self, other):
         if not isinstance(other, Alt):
@@ -106,9 +121,20 @@ class Alt(Node, ArgsRepr):
     def __iter__(self):
         yield from self.parts
 
+    def __len__(self):
+        return len(self.parts)
+
+    def __add__(self, other):
+        if not isinstance(other, Alt):
+            raise TypeError
+        return Alt(list(self.parts) + list(other.parts))
+
 
 class Part(Node, AttributeHolder):
-    def __init__(self, *, prime, pred=None, quant=None):
+    def __init__(self, *,
+                 prime,
+                 pred: Optional[Predicate] = None,
+                 quant: Optional[Quantifier | Repetition] = None):
         self.pred = pred
         self.prime = prime
         self.quant = quant
@@ -153,15 +179,18 @@ class Literal(Node, ArgsRepr):
             return NotImplemented
         return self.chars == other.chars
 
-    def __str__(self):
-        lit = ''.join(map(str, self.chars))
-        return f"Literal({lit!r})"
+    def __len__(self):
+        return len(self.chars)
 
     def __iter__(self):
         yield from self.chars
 
+    def __str__(self):
+        lit = ''.join(map(str, self.chars))
+        return f"Literal({lit!r})"
 
-class Class(Node, ArgsRepr):
+
+class Class(Node, ArgsRepr, Sized):
     def __init__(self, ranges: Iterable[Range]):
         self.ranges = ranges
 
@@ -172,6 +201,9 @@ class Class(Node, ArgsRepr):
         if not isinstance(other, Class):
             return NotImplemented
         return self.ranges == other.ranges
+
+    def __len__(self):
+        return len(self.ranges)
 
     def __iter__(self):
         yield from self.ranges
@@ -271,6 +303,25 @@ class Char(Node, ArgsRepr):
             return NotImplemented
         return self.code == other.code
 
+    def __lt__(self, other):
+        if not isinstance(other, Char):
+            return NotImplemented
+        return self.code < other.code
+
+    def __gt__(self, other):
+        if not isinstance(other, Char):
+            return NotImplemented
+        return self.code > other.code
+
+    def __ne__(self, other):
+        return not self.__eq(other)
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt(other)
+
+    def __ge__(self, other):
+        return self.__eq__(other) or self.__gt__(other)
+
     def __str__(self):
         if (c := chr(self.code)) and c in string.printable:
             return c
@@ -298,3 +349,21 @@ class GrammarVisitor:
                     self.visit(item, *args, **kwargs)
             else:
                 self.visit(value, *args, **kwargs)
+
+
+def common_prefix(lhs: Node, rhs: Node) -> Optional[list[Node]]:
+    parts = []
+    for a, b in zip(lhs, rhs):
+        if a != b:
+            return None
+        parts.append(a)
+    return parts
+
+
+def has_prefix(prefix: list[Node], node: Grammar | Expression) -> bool:
+    if len(node) < len(prefix):
+        return False
+    for a, b in zip(prefix, node):
+        if a != b:
+            return False
+    return True
