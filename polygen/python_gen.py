89f496e8ea3144387fa0b577955b20933c17fd29
glebzlat <mrkotofey@inbox.ru>
2024-05-05 19:08:12 +0500

    Refactor

--- polygen/python_gen.py
+++ polygen/python_gen.py
@@ -58,46 +58,49 @@ class PythonGenerator:
             self.put('return None')
 
     def gen_alt(self, alt, rule, alt_index):
-        items = []
         put_newline = len(alt) > 1
 
         self.put('if (', newline=put_newline)
         with self.indent():
             for i, part in enumerate(alt):
-                self.gen_part(part, items, alt_index, i, put_newline)
+                self.gen_part(part, i, put_newline)
         self.put('):', indent=put_newline)
 
         with self.indent():
             self.put('return True')
         self.put('self._reset(pos)')
 
-    def gen_part(self, part, items, alt_index, part_index, newline):
-        if type(part.prime) is Char:
-            fn, args = 'self._expectc', (part.prime,)
-        elif type(part.prime) is Identifier:
-            fn, args = f'self._{part.prime.string}', ()
-        elif type(part.prime) is String:
-            fn, args = 'self._expects', (part.prime,)
-        elif type(part.prime) is AnyChar:
-            fn, args = 'self._expectc', ()
+    def gen_part(self, part, part_index, newline):
+        parts = []
+
+        cond = ''
+        if type(part.pred) is Not:
+            parts += 'self._lookahead', False
+        elif type(part.pred) is And:
+            parts += 'self._lookahead', True
         else:
-            raise GeneratorError('unsupported node type', part)
+            cond = 'is not None'
 
         if type(part.quant) is ZeroOrOne:
-            fn, args = 'self._maybe', (fn, *args)
+            parts.append('self._maybe')
         elif type(part.quant) is ZeroOrMore:
-            fn, args = 'self._loop', (False, fn, *args)
+            parts += 'self._loop', False
         elif type(part.quant) is OneOrMore:
-            fn, args = 'self._loop', (True, fn, *args)
+            parts += 'self._loop', True
 
-        if type(part.pred) is Not:
-            fn, args = 'self._lookahead', (False, fn, *args)
-        elif type(part.pred) is And:
-            fn, args = 'self._lookahead', (True, fn, *args)
+        if type(part.prime) is Char:
+            parts += 'self._expectc', part.prime
+        elif type(part.prime) is Identifier:
+            parts.append(f'self._{part.prime.string}')
+        elif type(part.prime) is String:
+            parts += 'self._expects', part.prime
+        elif type(part.prime) is AnyChar:
+            parts.append('self._expectc')
+        else:
+            raise GeneratorError('unsupported node type', part.prime)
 
-        args = ', '.join(str(i) for i in args)
+        fn, args = parts[0], ', '.join(str(i) for i in parts[1:])
         call = f'{fn}({args})'
-        cond = 'is not None' if part.pred is None else ''
         op = 'and' if part_index else ''
 
         string = ' '.join(filter(None, (op, call, cond)))
