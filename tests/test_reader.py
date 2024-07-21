cb57b8370d4ff65851ea2c10ada185cafa26f231
glebzlat <mrkotofey@inbox.ru>
2024-07-21 08:12:48 +0500

    Use column for Token start and end positions
    Use Token __str__ as value
    
    Fix Reader unit test and add test for Token

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 import unittest
 
-from polygen.reader import Reader
+from polygen.parser import Reader, Token
 
 
 class ReaderTest(unittest.TestCase):
@@ -40,3 +40,28 @@ class ReaderTest(unittest.TestCase):
         _ = list(r)
 
         self.assertEqual(r.column, 6)
+
+    def test_token(self):
+        s = """abc
+def
+        """
+        r = Reader(s)
+
+        tokens = [
+            Token("a", line=1, start=0, end=1, filename="<string>"),
+            Token("b", line=1, start=1, end=2, filename="<string>"),
+            Token("c", line=1, start=2, end=3, filename="<string>"),
+            Token("\n", line=1, start=3, end=4, filename="<string>"),
+            Token("d", line=2, start=0, end=1, filename="<string>"),
+            Token("e", line=2, start=1, end=2, filename="<string>"),
+            Token("f", line=2, start=2, end=3, filename="<string>")
+        ]
+
+        self.assertEqual("a", tokens[0])
+
+        def token_attrs(tok: Token):
+            return (tok.value, tok.line, tok.start, tok.end, tok.filename)
+
+        result = list(r)
+        for a, b in zip(tokens, result):
+            self.assertEqual(token_attrs(a), token_attrs(b))
