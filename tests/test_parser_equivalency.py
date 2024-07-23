52289ad4810ac7a1b115b021291b7a84337d0789
glebzlat <mrkotofey@inbox.ru>
2024-07-23 08:03:13 +0500

    Add equivalency test metaclass that add test cases dynamically

--- tests/test_parser_equivalency.py
+++ tests/test_parser_equivalency.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from polygen.main import (
+    Backend,
     generate_parser,
     iterate_backend_files,
     BACKEND_DIRECTORY,
@@ -42,9 +43,59 @@ def get_data(clue_file):
         return evaluate(fin.read())
 
 
-class TestGenerateParser(unittest.TestCase):
+def normalize_name(s: str) -> str:
+    return s.replace(".", "_").replace("-", "_")
 
-    def test_generate(self):
+
+class EquivalencyTestMeta(type):
+
+    # Flip words in the name "test_case" because otherwise unittest thinks
+    # that it is a test method and tries to call it.
+    @staticmethod
+    def case_test(self,
+                  backend: Backend,
+                  backend_name: str,
+                  backend_output_dir: Path,
+                  test_case: Path):
+        backend.runner.find_deps()
+
+        tc_output_dir = backend_output_dir / test_case.name
+        tc_output_dir.mkdir(exist_ok=True)
+
+        grammar_file = test_case / "grammar.peg"
+        generate_parser(grammar_file=grammar_file,
+                        backend=backend,
+                        output_directory=tc_output_dir)
+
+        with backend.runner as runner:
+            for success_file in test_case.glob("*.success"):
+                clue_file = test_case / f"{success_file.name}.clue"
+                clue_data = get_data(clue_file)
+                exitcode, output = runner.run(success_file)
+                self.assertEqual(exitcode, 0, msg=f"Error: {output!r}")
+
+                try:
+                    result = evaluate(output)
+                except Exception as e:
+                    msg = (
+                        f"Error: "
+                        f"backend {backend_name}; "
+                        f"test_case {test_case.name}; "
+                        f"file {success_file.name}; "
+                        f"exception_type {type(e).__name__}; "
+                        f"message {str(e)!r}"
+                    )
+                    raise AssertionError(msg) from e
+
+                self.assertEqual(clue_data, result)
+
+            for failure_file in test_case.glob("*.failure"):
+                exitcode, output = backend.runner.run(failure_file)
+                self.assertNotEqual(exitcode, 0)
+
+    def __init__(self, name, bases, namespace):
+        setattr(self, "case_test", EquivalencyTestMeta.case_test)
+
         for backend in backends():
 
             gen = backend.generator
@@ -52,36 +103,16 @@ class TestGenerateParser(unittest.TestCase):
             backend_output_dir = TEST_DIR / backend_name
             backend_output_dir.mkdir(parents=True, exist_ok=True)
 
-            backend.runner.find_deps()
-
             for tc in TEST_CASES:
-                tc_output_dir = backend_output_dir / tc.name
-                tc_output_dir.mkdir(exist_ok=True)
+                test_name = f"test_{tc.name}_{normalize_name(backend_name)}"
 
-                grammar_file = tc / "grammar.peg"
-                generate_parser(grammar_file=grammar_file,
-                                backend=backend,
-                                output_directory=tc_output_dir)
+                def wrapper(self):
+                    self.case_test(
+                        backend, backend_name, backend_output_dir, tc)
 
-                for success_file in tc.glob("*.success"):
-                    clue_file = tc / f"{success_file.name}.clue"
-                    clue_data = get_data(clue_file)
+                setattr(self, test_name, wrapper)
+                wrapper.__name__ = test_name
 
-                    with backend.runner as runner:
-                        exitcode, output = runner.run(success_file)
-                    self.assertEqual(exitcode, 0, msg=f"Error: {output!r}")
 
-                    try:
-                        result = evaluate(output)
-                    except Exception as e:
-                        msg = (
-                            f"Error: "
-                            f"backend {backend_name}; "
-                            f"test_case {tc.name}; "
-                            f"file {success_file.name}; "
-                            f"exception_type {type(e).__name__}; "
-                            f"message {str(e)!r}"
-                        )
-                        raise AssertionError(msg) from e
-
-                    self.assertEqual(clue_data, result)
+class EquivalencyTest(unittest.TestCase, metaclass=EquivalencyTestMeta):
+    pass
