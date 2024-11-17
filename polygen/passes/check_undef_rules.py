689bbf1a30df174b75353c1ecf476acb357dbd45
glebzlat <mrkotofey@inbox.ru>
2024-11-17 15:29:08 +0500

    fix: visit ids only once, collect ids only

--- polygen/passes/check_undef_rules.py
+++ polygen/passes/check_undef_rules.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from polygen.translator import Translator, Context, TranslationError
 from polygen.visitor import GrammarPostVisitor, Context as Parents
 from polygen.node import (
@@ -13,31 +11,29 @@ from polygen.node import (
 class UndefRuleError(TranslationError):
     critical = False
 
-    def __init__(self, rule: Rule):
-        super().__init__("undefined rule {} with id {}")
-        self.args = (rule.id, rule)
+    def __init__(self, id: Id):
+        super().__init__("undefined rule {} at {pos}", pos=id.parse_info)
+        self.args = (id,)
 
 
 class CheckUndefRules(Translator, GrammarPostVisitor):
     """Find rules that are referenced but have no definitions"""
 
     def __init__(self):
-        self.named_items: defaultdict[Id, list[Rule]] = defaultdict(list)
+        self.ids: list[Id] = []
         self.rule_names: set[Id] = set()
 
     def translate(self, ctx: Context):
         self.visit(ctx.grammar)
 
-        if diff := set(self.named_items) - self.rule_names:
-            for i in diff:
-                for r in self.named_items[i]:
-                    ctx.error(UndefRuleError(r))
+        for i in self.ids:
+            if i not in self.rule_names:
+                ctx.error(UndefRuleError(i))
 
     def visit_Rule(self, node: Rule, parents: Parents):
         self.rule_names.add(node.id)
-        self._visit(node.expr, parents)
 
     def visit_Id(self, node: Id, parents: Parents):
         if type(parents[-1]) in (MetaRef, MetaRule):
             return
-        self.named_items[node].append(parents.rule)
+        self.ids.append(node)
