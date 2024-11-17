54b2273367ec0e4198f95a302c9f053a1455104d
glebzlat <mrkotofey@inbox.ru>
2024-11-17 12:10:37 +0500

    refactor: use argparse instead of click

--- polygen/__main__.py
+++ polygen/__main__.py
@@ -1,8 +1,7 @@
 import logging
 
 from pathlib import Path
-
-import click
+from argparse import ArgumentParser
 
 from polygen.main import (
     logger,
@@ -18,47 +17,59 @@ from unittest import TextTestRunner
 from polygen.equivalency.test import setUpUnittestSuite
 
 
-@click.group()
-def main():
-    pass
+argp = ArgumentParser()
+subp = argp.add_subparsers(title="commands")
 
 
-@main.command()
-@click.argument("grammar", required=True,
-                type=click.Path(exists=True, file_okay=True, readable=True,
-                                resolve_path=True, path_type=Path))
-@click.option("-b", "--backend", required=True, metavar="NAME",
-              help="code generator name")
-@click.option("-o", "--output", required=True,
-              type=click.Path(exists=True, file_okay=False, dir_okay=True))
-@click.option("-d", "--define", multiple=True)
-@click.option("-v", "--verbose", is_flag=True)
-def generate(backend, grammar, output, define, verbose):
-    file = find_backend_file(backend, [BACKEND_DIRECTORY])
-    backend_info = init_backend(file, config_options=define, verbose=verbose)
-    generate_parser(grammar_file=grammar,
+def cmd_generate_action(ns):
+    file = find_backend_file(ns.backend, [BACKEND_DIRECTORY])
+    backend_info = init_backend(file, config_options=ns.define,
+                                verbose=ns.verbose)
+
+    grammar_file = Path(ns.grammar)
+    if not (grammar_file.exists() and grammar_file.is_file()):
+        print(f"file {grammar_file} does not exist or not a file")
+        return 1
+
+    generate_parser(grammar_file=grammar_file,
                     backend=backend_info,
-                    output_directory=Path(output).resolve(),
-                    user_options=define,
-                    verbose=verbose)
+                    output_directory=Path(ns.output).resolve(),
+                    user_options=ns.define,
+                    verbose=ns.verbose)
 
 
-@main.command()
-@click.option("-b", "--backend", metavar="NAME")
-@click.option("-v", "--verbose", is_flag=True)
-def test(backend, verbose):
-    # I really need to came up with better idea how to configure the logger
-    # here, but I don't want to do it now.
-    # TODO: resolve logging crutch
-    if verbose:
+cmd_generate = subp.add_parser("generate")
+cmd_generate.add_argument("grammar")
+cmd_generate.add_argument("-b", "--backend", required=True,
+                          help="backend name")
+cmd_generate.add_argument("-o", "--output", required=True,
+                          help="output directory")
+cmd_generate.add_argument("-d", "--define", action="append", default=[])
+cmd_generate.add_argument("-v", "--verbose", action="store_true")
+cmd_generate.set_defaults(fn=cmd_generate_action)
+
+
+def cmd_test_action(ns):
+    if ns.verbose:
         logger.setLevel(logging.INFO)
     runner = TextTestRunner()
-    runner.run(setUpUnittestSuite(backend))
+    runner.run(setUpUnittestSuite(ns.backend))
+
+
+cmd_test = subp.add_parser("test")
+cmd_test.add_argument("-b", "--backend", help="backend name")
+cmd_test.add_argument("-v", "--verbose", action="store_true")
+cmd_test.set_defaults(fn=cmd_test_action)
+
+
+def main():
+    ns = argp.parse_args()
+    try:
+        return ns.fn(ns)
+    except (PolygenError, CodeGeneratorError) as e:
+        print(e)
+        return 1
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except (PolygenError, CodeGeneratorError) as e:
-        print(e)
-        exit(1)
+    exit(main())
