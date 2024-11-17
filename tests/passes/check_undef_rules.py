d923d73fd2a957aea36e0f6e33bfb732542373de
glebzlat <mrkotofey@inbox.ru>
2024-11-17 15:30:36 +0500

    tests: I forgot to uncomment test case

--- tests/passes/check_undef_rules.py
+++ tests/passes/check_undef_rules.py
@@ -7,16 +7,16 @@ from polygen.node import Grammar, Rule, Id, Expr, Alt, NamedItem, AnyChar
 
 class TestCheckUndefRules(unittest.TestCase):
 
-    # def test_success(self):
-    #     gram = Grammar([
-    #         Rule(Id('A'), Expr([Alt([NamedItem(None, Id('B'))])])),
-    #         Rule(Id('B'), Expr([Alt([NamedItem(None, AnyChar())])]))
-    #     ])
-    #
-    #     ctx = Context()
-    #     ctx.grammar = gram
-    #     p = CheckUndefRules()
-    #     p.translate(ctx)
+    def test_success(self):
+        gram = Grammar([
+            Rule(Id('A'), Expr([Alt([NamedItem(None, Id('B'))])])),
+            Rule(Id('B'), Expr([Alt([NamedItem(None, AnyChar())])]))
+        ])
+
+        ctx = Context()
+        ctx.grammar = gram
+        p = CheckUndefRules()
+        p.translate(ctx)
 
     def test_raises(self):
         id = Id('B')
